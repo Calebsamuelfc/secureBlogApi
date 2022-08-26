9c1a656e72e32eb92983dc5cd06f980dce846149
calebsamuelfc <samcalluis@gmail.com>
2022-08-26 14:20:31 +0100

    updating feature: pagination and sorting for branch

--- src/main/java/ocm/kay/secure_blog_api/service/PostService.java
+++ src/main/java/ocm/kay/secure_blog_api/service/PostService.java
@@ -1,12 +1,13 @@
 package ocm.kay.secure_blog_api.service;
 
 import ocm.kay.secure_blog_api.dto.PostDto;
+import ocm.kay.secure_blog_api.dto.PostResponse;
 
 import java.util.List;
 
 public interface PostService {
     PostDto createPost(PostDto postDto);
-    List<PostDto> getAllPosts(int pageNo, int pageSize);
+    PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir);
     PostDto getPostById(Long Id);
     PostDto updatePost(PostDto postDto, Long Id);
     void deletePost(Long id);
