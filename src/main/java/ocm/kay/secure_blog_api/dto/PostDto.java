d244b2896512edc164fbc838f5faf237a81eb527
calebsamuelfc <samcalluis@gmail.com>
2022-08-28 00:54:26 +0100

    added feature: api validations

--- src/main/java/ocm/kay/secure_blog_api/dto/PostDto.java
+++ src/main/java/ocm/kay/secure_blog_api/dto/PostDto.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @AllArgsConstructor
@@ -13,8 +15,13 @@ import java.util.Set;
 @Builder
 public class PostDto {
     private Long id;
+    @NotEmpty
+    @Size(min = 2, message = "Post title should have at least 2 characters")
     private String title;
+    @NotEmpty
+    @Size(min = 10, message = "Post description should have at least 10 characters")
     private String description;
+    @NotEmpty
     private String content;
     private Set<CommentDto> comments;
 }
